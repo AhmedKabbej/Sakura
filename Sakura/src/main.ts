af7285aad982538dc5b00de1e074ac6765a54891
Clémence Meyer <clemence.meyer@gmail.com>
2025-05-20 14:54:10 +0200

    feat(setup ts files)

--- Sakura/src/main.ts
+++ Sakura/src/main.ts
@@ -2,4 +2,19 @@ import { gsap } from "gsap";
     
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+class Manager {
+    constructor() {
+        this.initScrollManager()
+        this.initSoundManager()
+    }
+
+    initScrollManager() {
+    }
+
+    initSoundManager() {
+        //probably going to have a class for that
+    }
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {new Manager})
