a8fd8d43a8b281c3268479f9ed8e011b90a6dd1a
Clémence Meyer <clemence.meyer@gmail.com>
2025-05-21 01:04:08 +0200

    feat(lenis / smooth scroll)

--- Sakura/src/ScrollManager.ts
+++ Sakura/src/ScrollManager.ts
@@ -2,87 +2,109 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+import Lenis from "lenis";
+
 export class ScrollManager {
-    scrollContainer : HTMLElement;
-    frameContainers : NodeListOf<HTMLElement>;
-    scrollElements : {[index: number] : NodeListOf<HTMLElement>} = {};
-    scrollTimeline : GSAPTimeline;
+  scrollContainer: HTMLElement;
+  frameContainers: NodeListOf<HTMLElement>;
+  scrollElements: { [index: number]: NodeListOf<HTMLElement> } = {};
+  scrollTimeline: GSAPTimeline;
 
-    constructor() {
-        this.scrollContainer = document.querySelector("#main-container") as HTMLElement;
-        this.frameContainers = document.querySelectorAll(".scene-container") as NodeListOf<HTMLElement>;
-        this.frameContainers.forEach((frameContainer, idx) => {
-            this.scrollElements[idx] = frameContainer.querySelectorAll(".scene-element") as NodeListOf<HTMLElement>;
-        });
+  constructor() {
+    this.scrollContainer = document.querySelector(
+      "#main-container"
+    ) as HTMLElement;
+    this.frameContainers = document.querySelectorAll(
+      ".scene-container"
+    ) as NodeListOf<HTMLElement>;
+    this.frameContainers.forEach((frameContainer, idx) => {
+      this.scrollElements[idx] = frameContainer.querySelectorAll(
+        ".scene-element"
+      ) as NodeListOf<HTMLElement>;
+    });
 
-        this.scrollTimeline = gsap.timeline();
+    this.scrollTimeline = gsap.timeline();
+    this.initScroll();
 
-        this.initScroll()
+    this.initLenis();
+  }
+
+  initScroll() {
+    ScrollTrigger.create({
+      trigger: this.scrollContainer,
+      animation: this.scrollTimeline,
+      pin: true,
+      scrub: true,
+      end: window.innerWidth * this.frameContainers.length,
+    });
+
+    this.frameContainers.forEach((_, idx) => {
+      this.scrollWithin(idx);
+
+      if (idx === this.frameContainers.length - 1) return;
+      this.scrollTimeline.to(this.scrollContainer, {
+        //animate frame container to get to the center
+        x: "-=100vw",
+      });
+    });
+  }
+
+  scrollWithin(i: number) {
+    //add to scrollTrigger timeline anims for each scenes
+    switch (i) {
+      case 0:
+        this.scrollWithinFrameOne();
+        break;
+      case 1:
+        this.scrollWithinFrameTwo();
+        break;
+      case 2:
+        this.scrollWithinFrameThree();
+        break;
+      case 3:
+        this.scrollWithinFrameFour();
+        break;
+      default:
+        break;
     }
+  }
 
-    initScroll() {
-        const GAP = 512;
-        ScrollTrigger.create({
-            trigger: this.scrollContainer,
-            animation: this.scrollTimeline,
-            pin: true,
-            scrub: true,
-            end: (this.frameContainers[0].clientWidth + GAP) * (this.frameContainers.length-1)
-        })
+  scrollWithinFrameOne() {
+    this.scrollElements[0].forEach((scrollEl) => {
+      this.scrollTimeline.to(scrollEl, {
+        x: 100,
+      });
+    });
+  }
 
-        this.frameContainers.forEach((_, idx) => {
-            this.scrollWithin(idx)
+  scrollWithinFrameTwo() {
+    this.scrollElements[1].forEach((scrollEl) => {
+      this.scrollTimeline.to(scrollEl, {
+        x: -100,
+      });
+    });
+  }
 
-            if(idx === this.frameContainers.length -1) return;
-            this.scrollTimeline.to(this.scrollContainer, { //animate frame container to get to the center
-                x: '-=100vw',
-            });
-        })
-    }
+  scrollWithinFrameThree() {}
 
-    scrollWithin(i : number) {
-        //add to scrollTrigger timeline anims for each scenes
-        switch (i) {
-            case 0:
-                this.scrollWithinFrameOne()
-                break;
-            case 1:
-                this.scrollWithinFrameTwo()
-                break;
-            case 2:
-                this.scrollWithinFrameThree()
-                break;
-            case 3:
-                this.scrollWithinFrameFour()
-                break;
-            default:
-                break;
-        }
-    }
+  scrollWithinFrameFour() {}
 
-    scrollWithinFrameOne() {
-        this.scrollElements[0].forEach(scrollEl => {
-            this.scrollTimeline.to(scrollEl, {
-                x: 100
-            });
-        });
-    }
+  initLenis() {
+    // Initialize a new Lenis instance for smooth scrolling
+    const lenis = new Lenis();
 
-    scrollWithinFrameTwo() {
-        this.scrollElements[1].forEach(scrollEl => {
-            this.scrollTimeline.to(scrollEl, {
-                x: -100
-            });
-        });
-    }
+    // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+    lenis.on("scroll", ScrollTrigger.update);
 
-    scrollWithinFrameThree() {
+    // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+    // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+    });
 
-    }
-
-    scrollWithinFrameFour() {
-        
-    }
+    // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+    gsap.ticker.lagSmoothing(0);
+  }
 }
 
 // Reference for the scrollTrigger:
